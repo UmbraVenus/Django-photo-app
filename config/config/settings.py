423a98eaebac60fb038c04a93a0c0a4925a746ca
UmbraVenus <60438078+UmbraVenus@users.noreply.github.com>
2021-07-10 21:31:53 -0700

    Update settings.py

--- config/config/settings.py
+++ config/config/settings.py
@@ -16,6 +16,16 @@ from django.urls.conf import path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
