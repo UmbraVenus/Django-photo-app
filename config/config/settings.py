63f534505bd0dc7a5b869fa8d065efb8564fd7d5
UmbraVenus <60438078+UmbraVenus@users.noreply.github.com>
2021-07-10 22:23:33 -0700

    added white noise

--- config/config/settings.py
+++ config/config/settings.py
@@ -54,7 +54,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    #'django.middleware.WhiteNoiseMiddleware',
+    'django.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
